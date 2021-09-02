1af45a371668678e40a1637e17abd567c6a254b9
Adrian Kokai <adriankokai@gmail.com>
2021-09-02 06:24:46 +0200

    clean up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Blog from '../src/pages/Blog'
+import Portfolio from '../src/pages/Portfolio'
+import "materialize-css/dist/css/materialize.min.css";
+import BlogPost from './pages/BlogPost';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+  render() {
+    return (
+      <Router>
+          <Route exact path='/' component={Blog} />
+          <Route exact path='/portfolio' component={Portfolio} />
+          <Route exact path='/:title/' component={BlogPost} />
+      </Router>
+    )
+  }
 }
 
-export default App;
+export default App
